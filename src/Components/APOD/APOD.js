9ae50096627c0c1440f11a39df73d11068f93fc2
Juan Baires <juancbaires@gmail.com>
2018-11-24 21:55:04 -0500

    added a full screen modal to view the nasa image in full screen using areact component called react-modal

--- src/Components/APOD/APOD.js
+++ src/Components/APOD/APOD.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './APOD.css'
 import axios from 'axios'
+import expand from '../../images/expand.png'
+import Modal from 'react-modal'
 import { fbButton, pinterest } from 'vanilla-sharing';
 const API_KEY = process.env.REACT_APP_NASA_API_KEY
 const FACEBOOK_KEY = process.env.REACT_APP_FACEBOOK_KEY
 
-
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexDirection: 'column',
+        position: 'absolute'
+    }
+};
 //Setting state of the Apod componenet
 class APOD extends Component {
     state = {
@@ -14,6 +29,7 @@ class APOD extends Component {
         copyrightAuthor: '',
         date: '',
         title: '',
+        modalIsOpen: false
     }
 
     // Api Call to nasa's Pic of the day
@@ -28,6 +44,7 @@ class APOD extends Component {
                 title: res.data.title,
             })
         })
+        Modal.setAppElement('body')
     }
 
     //This function **TODO**
@@ -46,9 +63,26 @@ class APOD extends Component {
             url: 'https://npod.surge.sh/',
             description: this.state.about,
             media: this.state.imageHD,
+
         })
+
     }
 
+    //modal with picture opening function
+    openModal = () => {
+        this.setState({
+            modalIsOpen: true
+        });
+    }
+
+    afterOpenModal = () => {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    }
     render() {
 
         return (
@@ -57,8 +91,20 @@ class APOD extends Component {
                 <div className='search'>
                     <h1>{this.state.title}</h1>
                     <section className="nasa-card">
-
                         <img src={this.state.imageHD} alt="nasaApod" />
+                        <button className="full-screen" onClick={this.openModal}>[ {" "}]</button>
+                        <Modal
+                            isOpen={this.state.modalIsOpen}
+                            onAfterOpen={this.afterOpenModal}
+                            onRequestClose={this.closeModal}
+                            style={customStyles}
+                            contentLabel="Example Modal"
+                        >
+
+                            <h2 ref={subtitle => this.subtitle = subtitle}>{this.state.title}</h2>
+                            <img className="lightbox" src={this.state.imageHD} alt="nasashot" />
+                            <div className="close-button"><button onClick={this.closeModal}>X</button></div>
+                        </Modal>
                         <div className="about">
                             <h2>{this.state.copyrightAuthor}</h2>
                             <p>{this.state.date}</p>
